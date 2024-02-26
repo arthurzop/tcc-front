78f25b69959734f8fbf55a95eb89f6069633da84
arthurzop <medeirosartur48@gmail.com>
2024-02-26 17:24:28 -0300

    add: started the landing page

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import LandingPage from './pages/landing-page/landing-page';
+
+function App() {
+  return (
+    <div className="App">
+      <LandingPage></LandingPage>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/landing-page/landing-page.jsx
@@ -0,0 +1,29 @@
+import "../../App.css"
+import "./landing-page.css"
+
+// importando imagens
+import logoSenai from "../../assets/images/logo-senai.svg"
+import menuHamburguer from "../../assets/images/menu-svgrepo-com (1).svg"
+import heroImage from "../../assets/images/hero-image.svg"
+
+export default function LandingPage() {
+    return (
+        <body>
+            <header>
+                <img src={logoSenai} alt="logo" className="logo-senai" />
+                <div className="sub-header">
+                    <h1 className="header-name">Espaço Maker <span className="span-header">3D</span></h1>
+                    <img src={menuHamburguer} alt="menu hamburguer" className="menu-hamburguer" />
+                </div>
+            </header>
+            <section className="sec1"></section>
+            <div className="main">
+                <div className="hero-container">
+                    <h1 className="hero-title">Conheça o <br /> nosso espaço</h1>
+
+                    <img src={heroImage} alt="" />
+                </div>
+            </div>
+        </body>
+    )
+}
