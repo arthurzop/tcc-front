e04d334a64521c91debef08cf91520df195446a7
arthurzop <medeirosartur48@gmail.com>
2024-03-02 18:32:21 -0300

    feat: header/sidebar finished!

--- src/components/header-sidebar/header-sidebar.jsx
+++ src/components/header-sidebar/header-sidebar.jsx
@@ -1,14 +1,16 @@
 import "./header-sidebar.css";
 import { useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { Avatar } from "antd";
 import iconSair from "../../assets/images/icon-sair.svg";
-import { icons } from "antd/es/image/PreviewGroup";
 
 export default function HeaderSidebar() {
   const location = useLocation("");
 
-  const [isOpen, setIsOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [subMenuOpen, setsubMenuOpen] = useState(true);
+
+  const nav = useNavigate();
 
   return (
     <div className="hs-container">
@@ -21,17 +23,79 @@ export default function HeaderSidebar() {
         </div>
         <div className="horizontal-divider"></div>
         <div className="hs-sidebar-menu-container">
-          <h1 className="hs-menu-item">Agendamento</h1>
-          <h1 className="hs-menu-item">Dashboard</h1>
-          <h1 className="hs-menu-item">Gerenciamento</h1>
+          <h1 className="hs-menu-item" id="agendamento">
+            Agendamento
+          </h1>
+          <h1 className="hs-menu-item" id="dashboard">
+            Dashboard
+          </h1>
+          <h1
+            className="hs-menu-item"
+            id="gerenciamento"
+            onClick={() => {
+              setsubMenuOpen(!subMenuOpen);
+            }}
+          >
+            Gerenciamento
+          </h1>
         </div>
+        {subMenuOpen && (
+          <>
+            <div
+              className="hs-overlay"
+              onClick={() => {
+                setsubMenuOpen(!subMenuOpen);
+              }}
+            ></div>
+            <div className="sub-container">
+              <h1 className="sub-text">Estoque</h1>
+              <div className="modal-divider"></div>
+              <h1 className="sub-text">Pedidos</h1>
+              <div className="modal-divider"></div>
+              <h1 className="sub-text">Perfis</h1>
+            </div>
+          </>
+        )}
       </div>
       <div className="hs-header">
         <div className="hs-header-container">
           <h1 className="hs-title">{location.pathname.split(`/`)[1]}</h1>
-          <Avatar size={50}>A</Avatar>
+          <p
+            onClick={() => {
+              setMenuOpen(!menuOpen);
+            }}
+            className="hs-active"
+          >
+            <Avatar size={50} style={{ background: "darkgreen" }}>
+              A
+            </Avatar>
+          </p>
         </div>
       </div>
+      {menuOpen && (
+        <>
+          <div
+            className="hs-overlay"
+            onClick={() => {
+              setMenuOpen(false);
+            }}
+          ></div>
+          <div className="hs-modal-container">
+            <h1 className="modal-name">Manuela Vaz</h1>
+            <h3 className="modal-cargo">Admin</h3>
+            <div className="modal-divider"></div>
+            <div
+              className="modal-sair-container"
+              onClick={() => {
+                nav("/");
+              }}
+            >
+              <img src={iconSair} alt="" className="modal-sair-icon" />
+              <h1 className="modal-sair-text">Sair</h1>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
