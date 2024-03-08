c0c70a4f51f3ba82f407668875961cc82cdc9aad
oN0V41S <rafaelaugustonnovais@gmail.com>
2024-03-08 16:29:53 -0300

    feat: gerenciamento initial pages done

--- src/pages/gerenciamento/pedidos/pedidos.jsx
+++ src/pages/gerenciamento/pedidos/pedidos.jsx
@@ -1,10 +1,136 @@
-import HeaderSidebar from "../../../components/header-sidebar/header-sidebar"
-import "./pedidos.css"
+import HeaderSidebar from "../../../components/header-sidebar/header-sidebar";
+import "../gerenciamento.css";
+import { Table } from "antd";
+import crudDelete from "../../../assets/images/crud.svg";
+import { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export default function Pedidos() {
-    return(
-        <div className="section-body">
-        <HeaderSidebar/>
+
+  const location = useLocation("");
+  var pathname = location.pathname.split("/");
+  var pathname = location.pathname.replace("%20", " ").replace("/", "");
+
+  //data  para a tabela
+  const dataSource = [
+    {
+      key: "1",
+      usuario: "Peça",
+      nome: 32,
+      data: "10/12/2023",
+    },
+    {
+      key: "2",
+      usuario: "Vaso 2",
+      nome: 42,
+      data: "22/04/2022",
+    },
+    {
+      key: "2",
+      usuario: "Vaso 2",
+      nome: 42,
+      data: "22/04/2022",
+    },
+    {
+      key: "2",
+      usuario: "Vaso 2",
+      nome: 42,
+      data: "22/04/2022",
+    },
+    {
+      key: "2",
+      usuario: "Vaso 2",
+      nome: 42,
+      data: "22/04/2022",
+    },
+  ];
+
+  //colunas da tabela
+  const columns = [
+    {
+      title: "User",
+      dataIndex: "usuario",
+      key: "usuario",
+    },
+    {
+      title: "Nome",
+      dataIndex: "nome",
+      key: "nome",
+    },
+    {
+      title: "Data",
+      dataIndex: "data",
+      key: "data",
+    },
+    {
+      title: "#",
+      key: "deletar",
+      width: 100,
+      render: () => (
+        <a
+          onClick={() => {
+            setIsOpen(!isOpen);
+          }}
+        >
+          <img src={crudDelete} alt="" />
+        </a>
+      ),
+    },
+  ];
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleExcluir = async () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="section-body">
+      <HeaderSidebar />
+      <div className="section-container">
+        <h1 className="pedidos-title">{pathname}</h1>
+        <div className="table-container">
+          <Table
+            className="table"
+            dataSource={dataSource}
+            columns={columns}
+            scroll={{ y: "60%" }}
+          ></Table>
+        </div>
+      </div>
+      {isOpen && (
+        <div className="modal-container">
+          <div
+            className="hs-overlay"
+            onClick={() => {
+              setIsOpen(!isOpen);
+            }}
+          ></div>
+          <div className="modal-excluir-container">
+            <h1 className="modal-title">Tem Certeza?</h1>
+            <div className="modal-divider"></div>
+            <p className="modal=excluir-text">
+              Deseja realmente excluir este pedido? Essa ação é irreversível.
+            </p>
+            <div className="modal-btn-container">
+              <button
+                onClick={() => setIsOpen(!isOpen)}
+                className="modal-btn btn-cancelar"
+              >
+                Cancelar
+              </button>
+              <button
+                onClick={() => {
+                  handleExcluir();
+                }}
+                className="modal-btn btn-confirmar"
+              >
+                Confirmar
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
-    )
+  );
 }
--- src/pages/pedidos-usuario/pedidos.jsx
+++ src/pages/pedidos-usuario/pedidos.jsx
@@ -1,12 +1,10 @@
 import "./pedidos.css";
 import HeaderSidebar from "../../components/header-sidebar/header-sidebar";
 import { Table } from "antd";
-
 import crudDelete from "../../assets/images/crud.svg";
 import { useState } from "react";
 
 export default function PedidosUsuario() {
-
   //data  para a tabela
   const dataSource = [
     {
@@ -14,30 +12,6 @@ export default function PedidosUsuario() {
       usuario: "Peça",
       nome: 32,
       data: "10/12/2023",
-    },
-    {
-      key: "2",
-      usuario: "Vaso 2",
-      nome: 42,
-      data: "22/04/2022",
-    },
-    {
-      key: "2",
-      usuario: "Vaso 2",
-      nome: 42,
-      data: "22/04/2022",
-    },
-    {
-      key: "2",
-      usuario: "Vaso 2",
-      nome: 42,
-      data: "22/04/2022",
-    },
-    {
-      key: "2",
-      usuario: "Vaso 2",
-      nome: 42,
-      data: "22/04/2022",
     },
     {
       key: "2",
@@ -83,18 +57,26 @@ export default function PedidosUsuario() {
       key: "data",
     },
     {
-      title: '#',
-      key: 'deletar',
+      title: "#",
+      key: "deletar",
       width: 100,
-      render: () => <a onClick={() => { setIsOpen(!isOpen) }}><img src={crudDelete} alt="" /></a>
-    }
+      render: () => (
+        <a
+          onClick={() => {
+            setIsOpen(!isOpen);
+          }}
+        >
+          <img src={crudDelete} alt="" />
+        </a>
+      ),
+    },
   ];
 
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
 
   const handleExcluir = async () => {
-    setIsOpen(!isOpen)
-  }
+    setIsOpen(!isOpen);
+  };
 
   return (
     <div className="section-body">
@@ -106,25 +88,43 @@ export default function PedidosUsuario() {
             className="table"
             dataSource={dataSource}
             columns={columns}
-            scroll={{ y: '80%' }}
+            scroll={{ y: "60%" }}
           ></Table>
         </div>
       </div>
-      {isOpen &&
-
+      {isOpen && (
         <div className="modal-container">
-          <div className="hs-overlay" onClick={() => { setIsOpen(!isOpen) }}></div>
+          <div
+            className="hs-overlay"
+            onClick={() => {
+              setIsOpen(!isOpen);
+            }}
+          ></div>
           <div className="modal-excluir-container">
             <h1 className="modal-title">Tem Certeza?</h1>
             <div className="modal-divider"></div>
-            <p className="modal=excluir-text">Deseja realmente excluir este pedido? Essa ação é irreversível.</p>
+            <p className="modal=excluir-text">
+              Deseja realmente excluir este pedido? Essa ação é irreversível.
+            </p>
             <div className="modal-btn-container">
-              <button onClick={() => setIsOpen(!isOpen)} className="modal-btn btn-cancelar">Cancelar</button>
-              <button onClick={() => { handleExcluir() }} className="modal-btn btn-confirmar">Confirmar</button>
+              <button
+                onClick={() => setIsOpen(!isOpen)}
+                className="modal-btn btn-cancelar"
+              >
+                Cancelar
+              </button>
+              <button
+                onClick={() => {
+                  handleExcluir();
+                }}
+                className="modal-btn btn-confirmar"
+              >
+                Confirmar
+              </button>
             </div>
           </div>
         </div>
-      }
-    </div>  
+      )}
+    </div>
   );
 }
