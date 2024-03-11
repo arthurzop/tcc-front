70354cce4c347e2b89158323f7fbaa227783040b
Artur <125620379+arthurzop@users.noreply.github.com>
2024-03-11 17:11:48 -0300

    fix: updated cadastro logic

--- src/pages/login/cadastro.jsx
+++ src/pages/login/cadastro.jsx
@@ -1,15 +1,20 @@
 import React, { useState } from "react";
 import "./login.css";
 import "../../App.css";
+import { Form, Input } from "antd";
 
 import HeaderLogin from "../../components/header-login/header-login";
-import { Bounce, ToastContainer } from "react-toastify";
+import Select from "react-select";
+import { Bounce, ToastContainer, toast } from "react-toastify";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export default function Cadastro() {
+  const nav = useNavigate();
   const [cargo, setCargo] = useState("");
   const [email, setEmail] = useState("");
   const [nome, setNome] = useState("");
-  const [sala, setSala] = useState("");
+  const [turma, setTurma] = useState("");
   const [senha, setSenha] = useState("");
 
   const opcoes = [
@@ -19,86 +24,85 @@ export default function Cadastro() {
     { value: "funcionario", label: "Funcionário" },
   ];
 
-  const handleCadastro = async () => {};
+  const handleCadastro = async () => {
+    try {
+      const response = await axios.post("http://localhost:4000/cadastro-user", {
+        nome,
+        email,
+        cargo,
+        turma,
+        senha,
+      });
+      console.log(response.data); // Exemplo: Pode ser uma mensagem de sucesso
+      toast.success("Cadastro realizado com sucesso!");
+      nav("/login")
+    } catch (error) {
+      console.error("Erro ao cadastrar:", error);
+      toast.error("Erro ao cadastrar. Por favor, tente novamente.");
+    }
+  };
 
   return (
     <body className="body">
-      <ToastContainer
-        position="bottom-right"
-        autoClose={3000}
-        limit={1}
-        hideProgressBar={false}
-        newestOnTop
-        closeOnClick
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover={false}
-        transition={Bounce}
-        theme="dark"
-      />
+
 
       <HeaderLogin />
       <aside className="login-sidebar">
         <h1 className="login-title">Cadastro</h1>
         <div className="cadastro-container">
-          <div className="cadastro-form">
-            <div className="form-label">
+          <Form className="cadastro-form">
+            <Form.Item className="form-label">
               <label htmlFor="email">Nome: </label>
-              <input
+              <Input
                 className="cadastro-input"
                 placeholder="Digite seu nome:"
                 allowClear
                 onChange={(e) => setNome(e.target.value)}
               />
-            </div>
+            </Form.Item>
 
-            <div className="form-label">
+            <Form.Item className="form-label">
               <label htmlFor="email">Email: </label>
-              <input
+              <Input
                 className="cadastro-input"
                 placeholder="Digite seu email:"
                 allowClear
                 onChange={(e) => setEmail(e.target.value)}
               />
-            </div>
-            <div className="form-label">
+            </Form.Item>
+            <Form.Item className="form-label">
               <label htmlFor="email">Cargo: </label>
-              <select
+              <Select
                 options={opcoes}
                 isClearable
                 isSearchable
                 placeholder="Selecione seu cargo:"
-                onChange={(e) => setCargo(e.target.value)}
+                onChange={(e) => setCargo(e.value)}
                 className="cadastro-input"
               />
-            </div>
+            </Form.Item>
 
-            <div className="form-label">
+            <Form.Item className="form-label">
               <label htmlFor="email">Sala: </label>
-              <input
+              <Input
                 className="cadastro-input"
-                placeholder="Digite seu email:"
+                placeholder="Digite sua sala:"
                 allowClear
-                onChange={(e) => setSala(e.target.value)}
+                onChange={(e) => setTurma(e.target.value)}
               />
-            </div>
+            </Form.Item>
 
-            <div className="form-label">
+            <Form.Item className="form-label">
               <label htmlFor="senha">Senha: </label>
-              <input
+              <Input.Password
                 className="cadastro-input"
                 placeholder="••••••••••"
                 onChange={(e) => setSenha(e.target.value)}
               />
-            </div>
-          </div>
+            </Form.Item>
+          </Form>
         </div>
-        <button
-          className="login-button"
-          onClick={() => {
-            handleCadastro();
-          }}
-        >
+        <button className="login-button" onClick={handleCadastro}>
           Cadastrar
         </button>
         <div className="login-links-container">
